fe7fdfa3ed5783b6788731d81eaca12ac2fa11a1
Michael McClimon <michael@mcclimon.org>
2012-10-28 19:26:49 -0400

    Tests for new chord symbol objects.

--- randomTests.pl
+++ randomTests.pl
@@ -4,6 +4,7 @@ use strict;
 use v5.10;
 use TwoFive;
 
+=pod
 my $dm = TwoFive->new(2, 5, 0);
 #say $dm->toString;
 
@@ -21,10 +22,22 @@ say (TFT (2, 5, 0)->toString);
 say (THIRD (2, 5, 0)->toString);
 say $dm->TF->toString;
 
-say $dm->root;
-say $dm->third;
-say $dm->seventh;
+say for $dm->toList;
 
 say $dm->getQuality;
 say getQuality $dm;
 say getQuality (0, 4, 11);
+=cut
+
+my $conf = TwoFive->new(5, 9, 4);
+
+my @changes = ($conf);
+push @changes, $conf->SEVENTH->THIRD->T(11);
+push @changes, $changes[-1]->TF;
+push @changes, $changes[-1]->TF->SEVENTH->THIRD;
+push @changes, $changes[-1]->TF;
+push @changes, $changes[-1]->TF->SEVENTH->THIRD;
+
+say $_->getChordSymbol for @changes;
+say "---";
+say TwoFive->new("dm7")->TF->TF->T(4)->getChordSymbol;
